3574fee30d71232700f3345f190e12136d6ee9ed
Reagin Turner <rturnerafteragnes@gmail.com>
2021-10-06 08:21:17 -0400

    Testing out my site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
 // Event listener for search button
 var searchBtn = document.querySelector("#searchbtn")
 var searchInput = document.querySelector("#search")
+var searchHistory = document.querySelector("#search-history")
+var apiKey = ("adbfd834acb2ad11716672b9a64679a0")
+var city = $("#search").val().trim();  //Check if this works
 
-function getWeatherData(){
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
-    // console.log(fetch(requestUrl));
+function getCityInfo(){
+    
+}
+
+
+function getWeatherData(city){
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+    console.log(fetch(requestUrl));
 
   fetch(requestUrl)
     .then(function (response) {
@@ -16,17 +24,16 @@ function getWeatherData(){
       //looping over the fetch response and inserting the URL of your repos into a list
       for (var i = 0; i < data.length; i++) {
         //Create a list element
-        var listItem = document.createElement('li');
-        var repoLinks = document.createElement( "a");
+        var searchList = document.createElement('button');
+        //Add something for them to reclick on the button
 
         //Set the text of the list element to the JSON response's .html_url property
         // listItem.textContent = data[i].html_url;
-        repoLinks.textContent = data[i].html_url;
+        searchList.textContent = data[i];
 
-        var repoList = document.querySelector("section")
         //Append the li element to the id associated with the ul element.
-        repoList.appendChild(listItem);
-        listItem.appendChild(repoLinks);
+        searchHistory.appendChild(searchList);
+        
       }
     });
 }
