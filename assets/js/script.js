fbd54956760e4a359016937e78d500bd2e16e98f
Reagin Turner <rturnerafteragnes@gmail.com>
2022-01-12 13:40:02 -0500

    Tried fixing the layout; worked on calling the api

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,40 +2,60 @@
 var searchBtn = document.querySelector("#searchbtn")
 var searchInput = document.querySelector("#search")
 var searchHistory = document.querySelector("#search-history")
+var errorHolder = document.querySelector("#weather-card")
 var apiKey = ("adbfd834acb2ad11716672b9a64679a0")
-var city = $("#search").val().trim();  //Check if this works
-
-function getCityInfo(){
-    
-}
-
+  //Check if this works 
 
 function getWeatherData(city){
+    
     var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
-    console.log(fetch(requestUrl));
 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
       
+      
     })
     .then(function (data) {
         console.log(data)
-      //looping over the fetch response and inserting the URL of your repos into a list
-      for (var i = 0; i < data.length; i++) {
-        //Create a list element
-        var searchList = document.createElement('button');
-        //Add something for them to reclick on the button
-
-        //Set the text of the list element to the JSON response's .html_url property
-        // listItem.textContent = data[i].html_url;
-        searchList.textContent = data[i];
-
-        //Append the li element to the id associated with the ul element.
-        searchHistory.appendChild(searchList);
-        
-      }
+        // localStorage
     });
+
+    // //Current date and time 
+  
+    var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+    // var today = moment().format('dddd');
+    // // Show the daily weather data
+    var dailyWeather = document.querySelector("#daily-weather").setAttribute("style", "visibility: visible;")
+    var currentWeather = document.querySelector(".card-text")
+    currentWeather.innerHTML = "City: " +  city + '.<br />' + "Date:" + moment().format('MMMM Do YYYY, h:mm:ss a'); + '.<br />' 
+
+    var weeklyWeather = document.querySelector("#weekly-weather").setAttribute("style", "visibility: visible;")
 }
 
-searchBtn.addEventListener("click", getWeatherData);
+
+function getCityInfo(){
+  var city = $("#search").val().trim();
+  if(city){
+    getWeatherData(city);
+    historyLog(); // function to add the cities to the history
+  } else {
+    const errorDiv = document.createElement("div");
+    const errorMsg = document.createTextNode("Hi there and greetings!");
+    errorDiv.appendChild(errorMsg);
+    errorHolder.appendChild(errorDiv);
+  }
+  
+}
+
+//puts the city in the Search History
+function historyLog(){
+  //  if city has already been searched, don't re add,
+  // but if the city info is has results add the city name to the history
+}
+
+//fucntion that when a city in the search history is clicked, re displays the weather info
+
+
+searchBtn.addEventListener("click", getCityInfo);
